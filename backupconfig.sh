9f8f52b9fca1414dd194246ae1dabaabdceb8bbe
lbaitemple <lbai@temple.edu>
2022-05-10 18:30:06 -0400

    Update backupconfig.sh

--- backupconfig.sh
+++ backupconfig.sh
@@ -4,15 +4,15 @@ if [ $# -eq 1 ]; then
    if [ $1 = "backup" ]; then
       dir=`pwd`"/install/img_recognition/share/img_recognition/"
       dir1=${dir}"param"
-      cmd=`cp -rf ${dir1} ${dir1}.bak`
+      cmd=`rm -rf ${dir1}.bak && cp -rf ${dir1} ${dir1}.bak`
       dir2=${dir}"model"
-      cmd=`cp -rf ${dir2} ${dir2}.bak`
+      cmd=`rm -rf ${dir2}.bak &&  cp -rf ${dir2} ${dir2}.bak`
    elif [ $1 = "restore" ]; then 
       dir=`pwd`"/install/img_recognition/share/img_recognition/"
       dir1=${dir}"param"
-      cmd=`cp -rf ${dir1}.bak ${dir1}`
+      cmd=`rm -rf ${dir1} &&  cp -rf ${dir1}.bak ${dir1}`
       dir2=${dir}"model"
-      cmd=`cp -rf ${dir2}.bak ${dir2}`
+      cmd=`rm -rf ${dir2} &&  cp -rf ${dir2}.bak ${dir2}`
    else
       echo "$0 [backup|restore]"
    fi
