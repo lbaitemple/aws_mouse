09e0d54b1c0b45f781a0590c617ffef77eef27b8
lbaitemple <lbai@temple.edu>
2022-12-25 16:19:03 -0800

    Add files via upload

--- src/maze_demo/setup.py
+++ src/maze_demo/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['mouse_common'],
+    packages=['mouse_common', 'rospy_message_converter'],
     package_dir={'': 'src'}
 )
 setup(**d)
