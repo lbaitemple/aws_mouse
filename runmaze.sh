d399b314f88ba387d363d0caa6cdab136634c9bf
lbaitemple <lbai@temple.edu>
2023-02-09 21:03:26 -0500

    Create runmaze.sh

---
+++ runmaze.sh
@@ -0,0 +1,14 @@
+# run straight for 4.5
+rosrun maze_demo runner_template.py oforward 4.5
+
+# turn left for 90
+rosrun maze_demo runner_template.py turn 90
+
+# run straight for 2.1
+rosrun maze_demo runner_template.py oforward 2.1
+
+# turn left for 90
+rosrun maze_demo runner_template.py turn 90
+
+# run straight for 1.8
+rosrun maze_demo runner_template.py oforward 1.8
